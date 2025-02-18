784997de94725ad50122c3a48e67b86fe906c207
SAHIL141299 <sahilgohil141299@gmail.com>
2025-02-18 16:03:54 +0530

    minor details

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,24 @@
 import express, { Express, Request, Response } from 'express';
 import connectDB from './config/database';
 import  router  from './routes/userRoute';
+import path from 'path';
 const app = express();
 app.use(express.json());
 app.set('view engine','ejs')
+app.set('views', path.join(__dirname, 'views'));
 const PORT = 3030;
 
 app.use('/', router);
 
+connectDB();
+app.get('/', (req:Request, res: Response) => {
+  res.render('createtask');
+});
 
-app.get("/",(req:Request, res: Response)=>{
-    res.render("create")
-})
-app.get("/allTask",(req, res)=>{
+app.get("/allTask",(req:Request, res: Response)=>{
   res.render("allTasks")
 })
-connectDB();
+
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
